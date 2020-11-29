f4dff9bd3cd8ebab86888700a0dc313a5e4c5e0a
nisadrahman <nisadrehman@gmail.com>
2020-11-30 01:31:22 +0600

    Initial Commit

---
+++ src/main/java/com/hellonishad/LearnSpringBoot/topic/TopicService.java
@@ -0,0 +1,28 @@
+package com.hellonishad.LearnSpringBoot.topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
+            new Topic("spring", "Spring Framework", "Spring framework's Description"),
+            new Topic("java", "Core Java", "Core Java Description"),
+            new Topic("javascript", "javascript", "Javascript Description")
+    ));
+
+    public List<Topic> getAllTopics(){
+        return topics;
+    }
+
+    public Topic getTopic(String id){
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+}
