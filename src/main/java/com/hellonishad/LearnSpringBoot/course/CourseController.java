6021ec014bf1b641ff0e66b7673c6e7bea3a3629
nisadrahman <nisadrehman@gmail.com>
2020-12-02 00:03:22 +0600

    Courses module

---
+++ src/main/java/com/hellonishad/LearnSpringBoot/course/CourseController.java
@@ -0,0 +1,41 @@
+package com.hellonishad.LearnSpringBoot.course;
+
+import com.hellonishad.LearnSpringBoot.topic.Topic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @Autowired
+    private CourseService courseService;
+
+    @RequestMapping("/topics/{id}/courses")
+    public List<Course> getAllCourses(@PathVariable String id){
+        return courseService.getAllCourses(id);
+    }
+
+    @RequestMapping("/topics/{topicId}/courses/{courseId}")
+    public Course getCourse(@PathVariable String topicId,@PathVariable String courseId){
+        return courseService.getCourse(courseId);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/topics/{topicId}/courses")
+    public void addCourse(@RequestBody Course course, @PathVariable String topicId){
+        course.setTopic(new Topic(topicId,"",""));
+        courseService.addCourse(course);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{topicId}/courses/{courseId}")
+    public void updateCourse(@RequestBody Course course, @PathVariable String topicId, @PathVariable String courseId){
+        course.setTopic(new Topic(topicId, "", ""));
+        courseService.updateCourse(course);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{topicId}/courses/{id}")
+    public void deleteCourse(@PathVariable String id){
+        courseService.deleteCourse(id);
+    }
+}
